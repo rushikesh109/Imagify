55e94ce5d42d42c1a99a52cb965e9a027eabff58
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-11-21 19:01:31 +0530

    creating routes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+         <p>
+           Best text to image generator 
+         </p>
+      </div>  
+    </div>
+  )
+}
+
+export default Header
