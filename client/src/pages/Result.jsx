3d614707b004a4a2dc8e77e915255a65baf043f8
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-11-23 13:35:46 +0530

    animation and frontend done

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import { motion } from 'framer-motion'
+
 const Result = () => {
 
   const [image, setImage] = useState(assets.sample_img_1)
@@ -11,7 +13,12 @@ const Result = () => {
 
   }
   return (
-    <form onSubmit={onSubmitHandler}className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <motion.form 
+    initial={{ opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once:true}}
+    onSubmit={onSubmitHandler}className='flex flex-col min-h-[90vh] justify-center items-center'>
     <div>
        <div className='relative'>
                 <img src={image} alt="" className='max-w-sm rounded' />
@@ -40,7 +47,7 @@ const Result = () => {
 </div>
 }
 
-    </form>
+    </motion.form>
   )
 }
 
