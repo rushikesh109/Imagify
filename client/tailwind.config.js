89aa293ab92e42a3138bef64e23c6e3f473d3026
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-12-25 19:31:39 +0530

    changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js}'],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./index.html",
+  ],
   theme: {
     extend: {},
   },
